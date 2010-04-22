b3e011b15339701ce73f0c968474005568c9dd35
Matt Lacey <github@mrlacey.co.uk>
2010-04-22 21:52:28 +0100

    implementhing missing manual testing functionality

--- StringToNumberManualTesting/ManualTester.cs
+++ StringToNumberManualTesting/ManualTester.cs
@@ -24,37 +24,34 @@ namespace StringToNumberManualTesting
         {
             try
             {
-                NumberParser np;
+                Scale scaleToUse;
+                CultureInfo cultureToUse;
 
-                if (string.IsNullOrEmpty(CultureToUse.Text))
+                if (useShortScale.Checked)
                 {
-                    if (useShortScale.Checked)
-                    {
-                        np = new NumberParser();
-                    }
-                    else
-                    {
-                        np = new NumberParser(StringToNumber.Scale.Long);
-                    }
+                    scaleToUse = StringToNumber.Scale.Short;
                 }
                 else
                 {
-                    if (useShortScale.Checked)
-                    {
-                        np = new NumberParser(new CultureInfo(CultureToUse.Text));
-                    }
-                    else
-                    {
-                        np = new NumberParser(StringToNumber.Scale.Long, new CultureInfo(CultureToUse.Text));
-                    }
+                    scaleToUse = StringToNumber.Scale.Long;
                 }
+
+                if (string.IsNullOrEmpty(CultureToUse.Text))
+                {
+                    cultureToUse = CultureInfo.CurrentCulture;
+                }
+                else
+                {
+                    cultureToUse = new CultureInfo(CultureToUse.Text);
+                }
+
+                NumberParser np = new NumberParser(scaleToUse, cultureToUse);
 
                 bool result;
 
-                // TODO : add appropriate support for scales
-                // TODO : add other conversion types (14 total)
                 switch (TargetType.SelectedItem.ToString())
                 {
+                    #region ToByte
                     case "ToByte":
                         byte byteValue;
 
@@ -76,18 +73,12 @@ namespace StringToNumberManualTesting
                         }
                         else // if (AsExtensionMethod.Checked)
                         {
-                            if (string.IsNullOrEmpty(CultureToUse.Text))
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToByte(CultureInfo.CurrentCulture).ToString());
-                            }
-                            else
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToByte(new CultureInfo(CultureToUse.Text)).ToString());
-                            }
+                            MessageBox.Show(StringToConvert.Text.ToByte(cultureToUse).ToString());
                         }
 
                         break;
-
+                    #endregion
+                    #region ToSByte
                     case "ToSByte":
                         sbyte sbyteValue;
 
@@ -109,84 +100,12 @@ namespace StringToNumberManualTesting
                         }
                         else // if (AsExtensionMethod.Checked)
                         {
-                            if (string.IsNullOrEmpty(CultureToUse.Text))
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToSByte(CultureInfo.CurrentCulture).ToString());
-                            }
-                            else
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToSByte(new CultureInfo(CultureToUse.Text)).ToString());
-                            }
+                            MessageBox.Show(StringToConvert.Text.ToSByte(cultureToUse).ToString());
                         }
 
                         break;
-
-                    case "ToUInt16":
-                        UInt16 uint16Value;
-
-                        if (AsToXxxMethod.Checked)
-                        {
-                            MessageBox.Show(np.ToUInt16(StringToConvert.Text).ToString());
-                        }
-                        else if (AsTryToXxxMethod.Checked)
-                        {
-                            result = np.TryToUInt16(StringToConvert.Text, out uint16Value);
-
-                            MessageBox.Show(result + @" " + uint16Value);
-                        }
-                        else if (AsTryParseMethod.Checked)
-                        {
-                            result = np.TryParse(StringToConvert.Text, out uint16Value);
-
-                            MessageBox.Show(result + @" " + uint16Value);
-                        }
-                        else // if (AsExtensionMethod.Checked)
-                        {
-                            if (string.IsNullOrEmpty(CultureToUse.Text))
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToUInt16(CultureInfo.CurrentCulture).ToString());
-                            }
-                            else
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToUInt16(new CultureInfo(CultureToUse.Text)).ToString());
-                            }
-                        }
-
-                        break;
-
-                    case "ToUShort":
-                        ushort ushortValue;
-
-                        if (AsToXxxMethod.Checked)
-                        {
-                            MessageBox.Show(np.ToUShort(StringToConvert.Text).ToString());
-                        }
-                        else if (AsTryToXxxMethod.Checked)
-                        {
-                            result = np.TryToUShort(StringToConvert.Text, out ushortValue);
-
-                            MessageBox.Show(result + @" " + ushortValue);
-                        }
-                        else if (AsTryParseMethod.Checked)
-                        {
-                            result = np.TryParse(StringToConvert.Text, out ushortValue);
-
-                            MessageBox.Show(result + @" " + ushortValue);
-                        }
-                        else // if (AsExtensionMethod.Checked)
-                        {
-                            if (string.IsNullOrEmpty(CultureToUse.Text))
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToUShort(CultureInfo.CurrentCulture).ToString());
-                            }
-                            else
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToUShort(new CultureInfo(CultureToUse.Text)).ToString());
-                            }
-                        }
-
-                        break;
-
+                    #endregion
+                    #region ToInt16
                     case "ToInt16":
                         Int16 int16Value;
 
@@ -208,18 +127,39 @@ namespace StringToNumberManualTesting
                         }
                         else // if (AsExtensionMethod.Checked)
                         {
-                            if (string.IsNullOrEmpty(CultureToUse.Text))
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToInt16(CultureInfo.CurrentCulture).ToString());
-                            }
-                            else
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToInt16(new CultureInfo(CultureToUse.Text)).ToString());
-                            }
+                            MessageBox.Show(StringToConvert.Text.ToInt16(cultureToUse).ToString());
                         }
 
                         break;
+                    #endregion
+                    #region ToUInt16
+                    case "ToUInt16":
+                        UInt16 uint16Value;
 
+                        if (AsToXxxMethod.Checked)
+                        {
+                            MessageBox.Show(np.ToUInt16(StringToConvert.Text).ToString());
+                        }
+                        else if (AsTryToXxxMethod.Checked)
+                        {
+                            result = np.TryToUInt16(StringToConvert.Text, out uint16Value);
+
+                            MessageBox.Show(result + @" " + uint16Value);
+                        }
+                        else if (AsTryParseMethod.Checked)
+                        {
+                            result = np.TryParse(StringToConvert.Text, out uint16Value);
+
+                            MessageBox.Show(result + @" " + uint16Value);
+                        }
+                        else // if (AsExtensionMethod.Checked)
+                        {
+                            MessageBox.Show(StringToConvert.Text.ToUInt16(cultureToUse).ToString());
+                        }
+
+                        break;
+#endregion
+                    #region ToShort
                     case "ToShort":
                         short shortValue;
 
@@ -241,18 +181,39 @@ namespace StringToNumberManualTesting
                         }
                         else // if (AsExtensionMethod.Checked)
                         {
-                            if (string.IsNullOrEmpty(CultureToUse.Text))
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToShort(CultureInfo.CurrentCulture).ToString());
-                            }
-                            else
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToShort(new CultureInfo(CultureToUse.Text)).ToString());
-                            }
+                            MessageBox.Show(StringToConvert.Text.ToShort(cultureToUse).ToString());
                         }
 
                         break;
+                    #endregion
+                    #region ToUShort
+                    case "ToUShort":
+                        ushort ushortValue;
 
+                        if (AsToXxxMethod.Checked)
+                        {
+                            MessageBox.Show(np.ToUShort(StringToConvert.Text).ToString());
+                        }
+                        else if (AsTryToXxxMethod.Checked)
+                        {
+                            result = np.TryToUShort(StringToConvert.Text, out ushortValue);
+
+                            MessageBox.Show(result + @" " + ushortValue);
+                        }
+                        else if (AsTryParseMethod.Checked)
+                        {
+                            result = np.TryParse(StringToConvert.Text, out ushortValue);
+
+                            MessageBox.Show(result + @" " + ushortValue);
+                        }
+                        else // if (AsExtensionMethod.Checked)
+                        {
+                            MessageBox.Show(StringToConvert.Text.ToUShort(cultureToUse).ToString());
+                        }
+
+                        break;
+                    #endregion
+                    #region ToInt32
                     case "ToInt32":
                         Int32 int32Value;
 
@@ -274,18 +235,39 @@ namespace StringToNumberManualTesting
                         }
                         else // if (this.AsExtensionMethod.Checked)
                         {
-                            if (string.IsNullOrEmpty(CultureToUse.Text))
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToInt32(CultureInfo.CurrentCulture).ToString());
-                            }
-                            else
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToInt32(new CultureInfo(CultureToUse.Text)).ToString());
-                            }
+                            MessageBox.Show(StringToConvert.Text.ToInt32(cultureToUse).ToString());
                         }
 
                         break;
+                    #endregion
+                    #region ToUInt32
+                    case "ToUInt32":
+                        UInt32 uint32Value;
 
+                        if (this.AsToXxxMethod.Checked)
+                        {
+                            MessageBox.Show(np.ToUInt32(StringToConvert.Text).ToString());
+                        }
+                        else if (this.AsTryToXxxMethod.Checked)
+                        {
+                            result = np.TryToUInt32(StringToConvert.Text, out uint32Value);
+
+                            MessageBox.Show(result + @" " + uint32Value);
+                        }
+                        else if (this.AsTryParseMethod.Checked)
+                        {
+                            result = np.TryParse(StringToConvert.Text, out uint32Value);
+
+                            MessageBox.Show(result + @" " + uint32Value);
+                        }
+                        else // if (this.AsExtensionMethod.Checked)
+                        {
+                            MessageBox.Show(StringToConvert.Text.ToUInt32(cultureToUse).ToString());
+                        }
+
+                        break;
+                    #endregion
+                    #region ToInt
                     case "ToInt":
                         int intValue;
 
@@ -307,18 +289,40 @@ namespace StringToNumberManualTesting
                         }
                         else // if (this.AsExtensionMethod.Checked)
                         {
-                            if (string.IsNullOrEmpty(CultureToUse.Text))
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToInt(CultureInfo.CurrentCulture).ToString());
-                            }
-                            else
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToInt(new CultureInfo(CultureToUse.Text)).ToString());
-                            }
+                            MessageBox.Show(StringToConvert.Text.ToInt(cultureToUse).ToString());
                         }
 
                         break;
+                    #endregion
+                    #region ToUInt
+                    case "ToUInt":
+                        uint uintValue;
 
+                        if (this.AsToXxxMethod.Checked)
+                        {
+                            MessageBox.Show(np.ToUInt(StringToConvert.Text).ToString());
+                        }
+                        else if (this.AsTryToXxxMethod.Checked)
+                        {
+                            result = np.TryToUInt(StringToConvert.Text, out uintValue);
+
+                            MessageBox.Show(result + @" " + uintValue);
+                        }
+                        else if (this.AsTryParseMethod.Checked)
+                        {
+                            result = np.TryParse(StringToConvert.Text, out uintValue);
+
+                            MessageBox.Show(result + @" " + uintValue);
+                        }
+                        else // if (this.AsExtensionMethod.Checked)
+                        {
+                             MessageBox.Show(StringToConvert.Text.ToUInt(cultureToUse).ToString());
+                        }
+
+                        break;
+                    #endregion
+
+                    #region ToInt64
                     case "ToInt64":
                         Int64 int64Value;
 
@@ -340,18 +344,39 @@ namespace StringToNumberManualTesting
                         }
                         else // if (this.AsExtensionMethod.Checked)
                         {
-                            if (string.IsNullOrEmpty(CultureToUse.Text))
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToInt64(CultureInfo.CurrentCulture).ToString());
-                            }
-                            else
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToInt64(new CultureInfo(CultureToUse.Text)).ToString());
-                            }
+                            MessageBox.Show(StringToConvert.Text.ToInt64(scaleToUse, cultureToUse).ToString());
                         }
 
                         break;
+                    #endregion
+                    #region ToUInt64
+                    case "ToUInt64":
+                        UInt64 uint64Value;
 
+                        if (this.AsToXxxMethod.Checked)
+                        {
+                            MessageBox.Show(np.ToUInt64(StringToConvert.Text).ToString());
+                        }
+                        else if (this.AsTryToXxxMethod.Checked)
+                        {
+                            result = np.TryToUInt64(StringToConvert.Text, out uint64Value);
+
+                            MessageBox.Show(result + @" " + uint64Value);
+                        }
+                        else if (this.AsTryParseMethod.Checked)
+                        {
+                            result = np.TryParse(StringToConvert.Text, out uint64Value);
+
+                            MessageBox.Show(result + @" " + uint64Value);
+                        }
+                        else // if (this.AsExtensionMethod.Checked)
+                        {
+                            MessageBox.Show(StringToConvert.Text.ToUInt64(scaleToUse, cultureToUse).ToString());
+                        }
+
+                        break;
+                    #endregion
+                    #region ToLong
                     case "ToLong":
                         long longValue;
 
@@ -373,17 +398,38 @@ namespace StringToNumberManualTesting
                         }
                         else // if (this.AsExtensionMethod.Checked)
                         {
-                            if (string.IsNullOrEmpty(CultureToUse.Text))
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToLong(CultureInfo.CurrentCulture).ToString());
-                            }
-                            else
-                            {
-                                MessageBox.Show(StringToConvert.Text.ToLong(new CultureInfo(CultureToUse.Text)).ToString());
-                            }
+                            MessageBox.Show(StringToConvert.Text.ToLong(scaleToUse, cultureToUse).ToString());
                         }
 
                         break;
+                    #endregion
+                    #region ToULong
+                    case "ToULong":
+                        ulong ulongValue;
+
+                        if (this.AsToXxxMethod.Checked)
+                        {
+                            MessageBox.Show(np.ToULong(StringToConvert.Text).ToString());
+                        }
+                        else if (this.AsTryToXxxMethod.Checked)
+                        {
+                            result = np.TryToULong(StringToConvert.Text, out ulongValue);
+
+                            MessageBox.Show(result + @" " + ulongValue);
+                        }
+                        else if (this.AsTryParseMethod.Checked)
+                        {
+                            result = np.TryParse(StringToConvert.Text, out ulongValue);
+
+                            MessageBox.Show(result + @" " + ulongValue);
+                        }
+                        else // if (this.AsExtensionMethod.Checked)
+                        {
+                            MessageBox.Show(StringToConvert.Text.ToULong(scaleToUse, cultureToUse).ToString());
+                        }
+
+                        break;
+                    #endregion
 
                     default:
                         MessageBox.Show(@"Select conversion operation");
